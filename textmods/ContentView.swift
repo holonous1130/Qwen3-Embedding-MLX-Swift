220e8ef067d4a6a6eef07768d80efef78f879ea8
WANG YAXIONG <samwxx233@gmail.com>
2026-01-05 12:36:56 +0800

    Initial Commit

---
+++ textmods/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  textmods
+//
+//  Created by origin echo on 2026/1/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ textmods/textmodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  textmodsApp.swift
+//  textmods
+//
+//  Created by origin echo on 2026/1/5.
+//
+
+import SwiftUI
+
+@main
+struct textmodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
